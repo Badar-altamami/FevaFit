464482597d6320b232df5a3b18a471251e52bdaf
pranta <prantaacharjee647@gmail.com>
2022-10-10 21:11:14 +0600

    added preloader

--- src/Components/Feature/Feature.js
+++ src/Components/Feature/Feature.js
@@ -8,11 +8,18 @@ import cardBgImg from '../../assets/cardBgImg.png'
 import './Feature.css'
 import Modal from 'react-bootstrap/Modal';
 import ReactPlayer from 'react-player';
+import Spinner from 'react-bootstrap/Spinner';
 
 const Feature = ({ content }) => {
 
 
     function MyVerticallyCenteredModal(props) {
+        const [isLoading, setIsLoading] = useState(true);
+        useEffect(() => {
+            setTimeout(() => {
+                setIsLoading(false)
+            }, 2000);
+        }, [])
 
         let url = "https://player.vimeo.com/video/723315086?h=d23f6817e4&amp%3Bbadge=0&amp%3Bautopause=0&amp%3Bplayer_id=0&amp%3Bapp_id=58479&fbclid=IwAR0c10wOk_1tbEbv2vKb8z9hOV5bihV0cbfzllTsv5koHf5PYol-iPBGnqM"
 
@@ -36,6 +43,7 @@ const Feature = ({ content }) => {
                         content.dir === "rtl" ?
                             <Modal.Title
                                 id="contained-modal-title-vcenter">
+                                    
                                 <span style={{ color: '#6733B5', fontWeight: 'bold' }}>Feva Fit </span> ابدأ رحلة التغيير مع
                             </Modal.Title> :
                             <Modal.Title
@@ -47,16 +55,28 @@ const Feature = ({ content }) => {
 
 
                 </Modal.Header>
-                <Modal.Body>
- 
-                    <ReactPlayer width="100%" controls url={url}
-                        onReady={() => console.log("ready")}
-                        onStart={() => console.log('onStart')}
-                    >
-                        
-                    </ReactPlayer>
 
+
+                <Modal.Body>
+                    {
+                        isLoading === true ? <div className='text-center'>
+                           <p>{content.dir === "rtl" ?"فضلا انتظر لحظة":"Please wait a moment."}</p>
+                            <Spinner animation="grow" />
+                            <Spinner animation="grow" className="mx-2" />
+                            <Spinner animation="grow" />
+                        </div>
+
+                            :
+
+                            <ReactPlayer width="100%" controls url={url}
+                                onReady={() => console.log("ready")}
+                                onStart={() => console.log('onStart')}
+                            >
+
+                            </ReactPlayer>
+                    }
                 </Modal.Body>
+
             </Modal>
         );
     }
@@ -77,7 +97,7 @@ const Feature = ({ content }) => {
             <div className='container py-5'>
                 <Row className="serviceCardGroup pt-4 g-4">
                     <div className='col-md-4 col-sm-12 text-center'>
-                        <div className='mx-4 p-5 shadow-sm rounded card1'  data-aos="zoom-in-right" data-aos-duration="3000">
+                        <div className='mx-4 p-5 shadow-sm rounded card1' data-aos="zoom-in-right" data-aos-duration="3000">
                             <div className='py-4'>
                                 <img src={cardIcon1} alt="" />
                             </div>
@@ -87,14 +107,14 @@ const Feature = ({ content }) => {
                     </div>
                     <div className='col-md-4 col-sm-12 cardn text-center'>
                         <div className='cardBgImg'>
-                            <img src={cardBgImg} alt=""/>
+                            <img src={cardBgImg} alt="" />
                         </div>
                         <div className='mx-4 p-5 shadow-sm rounded card2' data-aos="zoom-in-up" data-aos-duration="3000">
                             <div className='py-2' >
-                            <div className='ripple'>
-                              <img className='img-fluid player' src={cardIcon2} alt="" onClick={() => setModalShow(true)} />
-                                <div className='shadow-img'></div>
-                              </div>
+                                <div className='ripple'>
+                                    <img className='img-fluid player' src={cardIcon2} alt="" onClick={() => setModalShow(true)} />
+                                    <div className='shadow-img'></div>
+                                </div>
 
 
                                 <MyVerticallyCenteredModal
@@ -107,7 +127,7 @@ const Feature = ({ content }) => {
                         </div>
                     </div>
                     <div className='col-md-4 col-sm-12 text-center'>
-                        <div className='mx-4 p-5 shadow-sm rounded card3'  data-aos="zoom-in-left" data-aos-duration="3000">
+                        <div className='mx-4 p-5 shadow-sm rounded card3' data-aos="zoom-in-left" data-aos-duration="3000">
                             <div className='py-4'>
 
 
